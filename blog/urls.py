4c1b5ea6f99b2c7a95d6277d0323c23dfe156c19
Mykhaylo Chornenkyy <xipypggg@gmail.com>
2023-01-28 13:32:22 +0000

    Finish crispy forms

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ import blog.views
 urlpatterns = [
     # other patterns
     path('', blog.views.index),
+    path('samples/', blog.views.samples, name='blog-samples'),
     path('post/<slug>/', blog.views.post_detail, name="blog-post-detail"),
 ]
