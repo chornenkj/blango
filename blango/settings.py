489ce4c5d118f43990f15534f7e5ce9f1af08bc3
Mykhaylo Chornenkyy <xipypggg@gmail.com>
2023-02-02 14:32:05 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -52,7 +52,8 @@ class Dev(Configuration):
         'blog.apps.BlogConfig',
 
         'crispy_forms',
-        'crispy_bootstrap5'
+        'crispy_bootstrap5',
+        
     ]
 
     MIDDLEWARE = [
@@ -148,6 +149,15 @@ class Dev(Configuration):
         {
             'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
         },
+    ]
+
+
+    PASSWORD_HASHERS = [
+        
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.Argon2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     ]
 
 
