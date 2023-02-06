b1d06cf526e4e5a84644da655308dcb3b1298601
Mykhaylo Chornenkyy <xipypggg@gmail.com>
2023-02-06 16:44:08 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -19,6 +19,7 @@ class Dev(Configuration):
     # Build paths inside the project like this: BASE_DIR / 'subdir'.
     BASE_DIR = Path(__file__).resolve().parent.parent
 
+    AUTH_USER_MODEL = "blango_auth.User"
 
     # Quick-start development settings - unsuitable for production
     # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -49,6 +50,7 @@ class Dev(Configuration):
         'django.contrib.messages',
         'django.contrib.staticfiles',
 
+        'blango_auth.apps.BlangoAuthConfig',
         'blog.apps.BlogConfig',
 
         'crispy_forms',
