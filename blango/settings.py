a41f2573ce16c371439a59b5e00f0b2342a1244d
Mykhaylo Chornenkyy <xipypggg@gmail.com>
2023-02-09 08:06:21 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -50,6 +50,7 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
 
         'blog.apps.BlogConfig',
@@ -58,6 +59,11 @@ class Dev(Configuration):
         'crispy_bootstrap5',
 
         'debug_toolbar',
+
+        'allauth',
+        'allauth.account',
+        'allauth.socialaccount',
+        'allauth.socialaccount.providers.google',
         
     ]
 
@@ -199,6 +205,13 @@ class Dev(Configuration):
     ACCOUNT_ACTIVATION_DAYS = 7
 
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+    SITE_ID = 1
+
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
 
 class Prod(Dev):
